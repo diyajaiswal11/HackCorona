89bf2366fc2a54145bc7671f7cc14d4d7cda92c7
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-04-04 04:30:20 +0530

    precautions page

--- hackcorona/passgen/urls.py
+++ hackcorona/passgen/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('epass/<uniquenumber>/', views.index,name='epass'),
     path('pdf_view/<uniquenumber>/', views.ViewPDF.as_view(), name="pdf_view"),
     path('pdf_download/<uniquenumber>/', views.DownloadPDF.as_view(), name="pdf_download"),
+    path('faq/',views.faqpage,name='faqpage'),
+    path('precautions/',views.precautions,name='precautions'),
 ]
