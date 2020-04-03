b9783b1bda69dd0093e016bf2f6286de2a8f06ef
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-04-04 01:55:13 +0530

    home page

--- hackcorona/hackcorona/settings.py
+++ hackcorona/hackcorona/settings.py
@@ -121,4 +121,9 @@ MEDIA_URL='/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
