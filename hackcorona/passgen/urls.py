b414532a19879692cee62bc4f16147cafe3c3f1a
todi-2000 <manshitodi@gmail.com>
2020-04-03 22:57:13 +0530

    changes in views

--- hackcorona/passgen/urls.py
+++ hackcorona/passgen/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('fillform/',views.fillform,name='fillform'),
     path('epass/<uniquenumber>/', views.index,name='epass'),
-    path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view"),
-    path('pdf_download/', views.DownloadPDF.as_view(), name="pdf_download"),
+    path('pdf_view/<uniquenumber>/', views.ViewPDF.as_view(), name="pdf_view"),
+    path('pdf_download/<uniquenumber>/', views.DownloadPDF.as_view(), name="pdf_download"),
 ]
