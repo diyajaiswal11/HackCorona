d3de26bc473db0a29f3e96b77c23c6cc8869a588
todi-2000 <manshitodi@gmail.com>
2020-04-08 02:09:00 +0530

    changes in settings

--- hackcorona/hackcorona/settings.py
+++ hackcorona/hackcorona/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'vka^v(g0e&=vj0_v$1v7s00p@s$1tm#$%$p&+r+ql7k_6hvfu6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
